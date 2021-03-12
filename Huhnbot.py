34f66ff08e465e028a3d7527e1ba573f001e3ec1
Bommlon <52289712+Bommlon@users.noreply.github.com>
2021-03-12 16:44:59 +0100

    Uploaded Huhnbot.py

---
+++ Huhnbot.py
@@ -0,0 +1,136 @@
+#https://discord.com/oauth2/authorize?client_id=813038801211228201&scope=bot
+
+import discord
+
+class RoleReactClient(discord.Client):  # stolen from https://github.com/Rapptz/discord.py/blob/master/examples/reaction_roles.py
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.role_message_id = 819957058719711232  # ID of message that can be reacted to to add role
+        self.emoji_to_role = {
+            733382938657554555: 736544036151623690,  # ID of role associated with partial emoji object 'spacehuhn_head' -> Huhn
+            #804771444130840617: 818520356754292737  # ID of role associated with partial emoji object 'stonk' -> testBot
+        }
+
+    async def on_raw_reaction_add(self, payload):
+        """Gives a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about
+        if payload.message_id != self.role_message_id:
+            return
+
+        try:
+            #print(payload.emoji.id)
+            role_id = self.emoji_to_role[payload.emoji.id]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        guild = self.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        try:
+            # Finally add the role
+            await payload.member.add_roles(role)
+            print(str(role) + " given to " + str(payload.member.name))
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
+
+    async def on_raw_reaction_remove(self, payload):
+        """Removes a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about
+        if payload.message_id != self.role_message_id:
+            return
+
+        try:
+            role_id = self.emoji_to_role[payload.emoji.id]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        guild = self.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        member = guild.get_member(payload.user_id)
+        if member is None:
+            # Makes sure the member still exists and is valid
+            return
+
+        try:
+            # Finally, remove the role
+            await member.remove_roles(role)
+            print(str(role) + " removed from " + str(member.name))
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
+
+# This bot requires the members and reactions intents.
+intents = discord.Intents.default()
+intents.members = True
+
+client = RoleReactClient(intents=intents)
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    badWords = ["fuck", "shit", "nigg", "fag", "cunt", "sex"] #list of unwanted profanities
+    noBadWord = True
+
+    for word in badWords:
+        if word in message.content.lower():
+            await message.channel.send(str(message.author.mention) + " HEY, please keep it family friendly!")
+            noBadWord = False
+            break
+
+    #only proceed if author didn't swear
+    if noBadWord:
+        if message.author == client.user:
+            return
+
+        elif client.user.mentioned_in(message) and ("shut up" in message.content):
+            if discord.utils.find(lambda r: r.name == "Moderator", message.guild.roles) in message.author.roles:
+                await client.get_channel(733343988261584896).send("Huhnbot deactivated by " + str(message.author))  # ID for moderation channel                
+                exit()
+            else:
+                await message.channel.send("lol nope")
+
+        #elif client.user.mentioned_in(message) and ("post" in message.content):
+        #    if discord.utils.find(lambda r: r.name == "Moderator", message.guild.roles) in message.author.roles:
+        #        await client.get_channel(733343036938911795).send(":warning: Click on the spacehuhn emoji below to become a server member, unlock all channels and connect with the community")  # ID for welcome channel
+
+        elif client.user.mentioned_in(message) and ("hello" in message.content):
+            await message.channel.send("hi :smile:")
+
+        elif client.user.mentioned_in(message) and ("sus" in message.content):
+            await message.channel.send(discord.utils.find(lambda r: r.name == "suspicious", message.guild.emojis))
+        
+        elif client.user.mentioned_in(message) and (("like waffles" in message.content) or ("love waffles" in message.content)):
+            await message.channel.send("yes" + str(discord.utils.find(lambda r: r.name == "partyhuhn", message.guild.emojis)))
+
+        elif client.user.mentioned_in(message) and ("bad bot" in message.content):
+            await message.channel.send(":cry:")
+
+        elif client.user.mentioned_in(message) and ("good bot" in message.content):
+            await message.channel.send(":blush:")
+
+        elif client.user.mentioned_in(message):
+            await message.channel.send("Hmmm?")
+
+client.run("token")
